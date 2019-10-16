659daad24fd08564e5e918e701a1bb250be6ad03
John Bampton <jbampton@users.noreply.github.com>
2019-10-17 07:23:08 +1000

    Update random-fuchsia-logo.sh

--- shell/random-fuchsia-logo.sh
+++ shell/random-fuchsia-logo.sh
@@ -2,6 +2,7 @@
 
 mkdir -p random
 cd random || exit
+# shellcheck disable=SC2004
 frandom=$((RANDOM%307))
 # shellcheck disable=SC1073
 curl -O https://raw.githubusercontent.com/fuchsia-programming/fuchsia-programming.github.io/master/assets/images/logos/fuchsia-$(( frandom  )).png
@@ -11,8 +12,9 @@ then
   echo "Logo not found"
   exit 1
 else
+  ls
   echo "Logo fuchsia-$((frandom)).png found"
-  if [ "$(uname -s)" == "Linux" ]; then
+  if [[ "$(uname -s)" == "Linux" ]]; then
      echo "Linux"
      feh fuchsia-$((frandom)).png
   fi
