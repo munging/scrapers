75cd2c94841dad680610f8c5fe07b7520c2c0916
Restyled.io <commits@restyled.io>
2019-10-29 03:35:44 +0000

    Restyled by shfmt

--- shell/github-max-per-day-sed.sh
+++ shell/github-max-per-day-sed.sh
@@ -2,4 +2,3 @@
 
 # print the maximum number of GitHub contributions in one day from the last year
 curl https://github.com/"$1" | sed -n 's/.*data-count="//p' | sed 's/".*//' | sort -n | uniq | tail -n 1
-
